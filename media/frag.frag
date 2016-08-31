aef648f265e52d5f732c438fd0b8a851d4f0bb16
joelschum@gmail.com <joelschum@gmail.com>
2016-08-31 04:06:32 +0200

    area lights and notes

--- media/frag.frag
+++ media/frag.frag
@@ -3,26 +3,64 @@
 in VSOUT {
     in vec2 texCoord;
     in vec3 normal;
-    in vec3 eye;
+    in vec3 eye; // The inverse = position
 } vsOut;
 
 out vec4 color;
 
 uniform vec3 lightDir; // view space
 
+uniform vec3 areaLightPos;
+uniform vec2 areaLightSize;
+uniform mat3 areaLightBase;
+uniform float ambient = 0.0;
+
+vec3 boxIn(vec3 P) {
+    vec3 inLightBase = transpose(areaLightBase) * (P - areaLightPos); // fragment position in light space
+    inLightBase.xy = clamp(inLightBase.xy, -areaLightSize/2.0, areaLightSize/2.0);
+    inLightBase.z = 0; // zero component along normal/area light direction
+    return areaLightPos + areaLightBase * inLightBase;
+}
+
 void main() {
     vec3 N = normalize(vsOut.normal); // renormalize because of interpolation?
     vec3 E = normalize(vsOut.eye);
     vec3 L = lightDir;
-    vec3 reflectDir = reflect(-L, N);
+    float L_atten = 1.0; // directional
 
+    vec3 P = -vsOut.eye; // fragment position in camera space
+    vec3 lightPos = boxIn(P);
+    L = lightPos - P;
+    float L_dist = length(L);
+    L = normalize(L);
+    float a = 0.00001;
+    float b = 0.00001;
+    L_atten = 1.0 / (1.0 + a * L_dist + b * L_dist*L_dist);
+    //facing the plane -> brighter
+    L_atten *= abs(dot(L, -areaLightBase[2]));
+    if(isnan(L_atten)) L_atten = 1.0;
+    //L_atten *= step(0.0, dot(-L, areaLightBase[2]));
+
+    vec3 albedo = vec3(1.0, 1.0, 1.0);
     vec3 diffColor = vec3(1.0, 1.0, 1.0);
-    vec3 specColor = vec3(1.0, 1.0, 1.0) * 1.0;
-    vec3 ambiColor = vec3(1.0, 1.0, 1.0) * 0.05;
-    float shininess = 256.0;
+    vec3 specColor = vec3(1.0, 1.0, 1.0);
+    vec3 ambiColor = vec3(1.0, 1.0, 1.0) * ambient;
+    float shininess = 128.0;
 
-    float lambert = max(dot(L, N), 0.0) + smoothstep(0.35, 1.0, 1.0 - max(dot(E, N), 0.0)) * 0.8;
-    float specular = pow(max(dot(E, reflectDir), 0.0), shininess);
-    vec4 albedo = vec4(1.0, 1.0, 1.0, 1.0);
-    color = vec4(albedo.rgb * (lambert * diffColor + ambiColor) + specular * specColor, 1.0);
+    float rimLight = smoothstep(0.35, 1.0, 1.0 - max(dot(E, N), 0.0)) * 0.8;
+    rimLight = 0.0;
+    float lambert = max(dot(L, N), 0.0) + rimLight;
+    lambert *= L_atten;
+
+    vec3 reflectEye = normalize(reflect(-E, N));
+    // project this reflected vector onto the plane (ray-plane intersection)
+    // wiki: line_pos + line_dir * dot(plane_pos - line_pos, plane_normal) / dot(line_dir, plane_normal)
+    vec3 projected = P + reflectEye * dot(areaLightPos - P, areaLightBase[2]) / dot(reflectEye, areaLightBase[2]);
+    vec3 projectedDir = normalize(boxIn(projected) - P);
+    float specular = pow(max(dot(reflectEye, projectedDir), 0.0), shininess);
+    //float specular = pow(max(dot(-L, reflectEye), 0.0), shininess);
+    specular *= L_atten;
+    //specular = 0.0;
+
+    color = vec4(albedo * (lambert * diffColor + ambiColor) + specular * specColor, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,20 @@
 #include <ngn/ngn.hpp>
 
 std::vector<ngn::Mesh*> meshes;
-ngn::Mesh* boxMesh;
+ngn::Mesh* lightMesh;
 ngn::VertexFormat vFormat;
 
 ngn::ShaderProgram *shader;
-glm::mat4 projectionMatrix;
 ngn::PerspectiveCamera camera(glm::radians(45.0f), 1.0f, 0.1f, 1000.0f);
 glm::vec4 light = glm::vec4(1.0, 0.5, 1.0, 1.0);
 
+glm::vec3 areaLightPos = glm::vec3(40.0f, 7.0f, 0.0f);
+glm::vec2 areaLightSize = glm::vec2(20.0f, 10.0f);
+glm::vec3 areaLightDir = glm::vec3(-1.0f, 0.0f, 0.0f);
+
 bool initGL() {
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glEnable(GL_CULL_FACE);
+    //glEnable(GL_CULL_FACE);
     glEnable(GL_DEPTH_TEST);
 
     shader = new ngn::ShaderProgram();
@@ -32,9 +35,15 @@ bool initGL() {
     vFormat.add(ngn::AttributeType::POSITION, 3, ngn::AttributeDataType::F32);
     vFormat.add(ngn::AttributeType::NORMAL, 3, ngn::AttributeDataType::F32);
 
-    //boxMesh = ngn::boxMesh(0.5f, 0.5f, 0.5f, vFormat);
-    //boxMesh = ngn::sphereMesh(0.5f, 40, 40, vFormat);
     meshes = ngn::assimpMeshes("media/ironman.obj", vFormat);
+    meshes.push_back(ngn::planeMesh(1000.0f, 1000.0f, 1, 1, vFormat));
+    meshes.push_back(ngn::boxMesh(100.0f, 100.0f, 100.0f, vFormat));
+
+    lightMesh = ngn::planeMesh(1.0f, 1.0f, 1, 1, vFormat);
+    lightMesh->transform(glm::mat4(glm::vec4(1.0f, 0.0f, 0.0f, 0.0f),
+                                   glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
+                                   glm::vec4(0.0f, 1.0f, 0.0f, 0.0f),
+                                   glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)));
 
     camera.setPosition(glm::vec3(glm::vec3(0.0f, 0.0f, 3.0f)));
 
@@ -55,7 +64,7 @@ void moveCamera(float dt) {
     int numkeys = 0;
     const Uint8 *keyState = SDL_GetKeyboardState(&numkeys);
 
-    float speed = 6.0 * dt;
+    float speed = 10.0 * dt;
     glm::vec3 move(0.0);
     move.x = ((int)keyState[SDL_SCANCODE_D] - (int)keyState[SDL_SCANCODE_A]);
     move.y = ((int)keyState[SDL_SCANCODE_R] - (int)keyState[SDL_SCANCODE_F]);
@@ -89,12 +98,40 @@ void render(float dt) {
     light = light * glm::rotate(glm::mat4(), 1.0f * dt, glm::vec3(0.0f, 1.0, 0.0f));
     glm::vec3 lightDir = glm::normalize(glm::mat3(camera.getViewMatrix()) * glm::vec3(light));
 
+    areaLightDir = glm::vec3(-1.0f, -2.0 * (glm::sin(ngn::getTime()) * 0.5f + 0.5f), 0.0f);
+
+    glm::mat3 areaLightBase;
+    areaLightBase[2] = glm::normalize(areaLightDir);
+    areaLightBase[0] = glm::normalize(glm::cross(areaLightBase[2], glm::vec3(0.0f, 1.0f, 0.0f)));
+    areaLightBase[1] = glm::normalize(glm::cross(areaLightBase[0], areaLightBase[2]));
+
+
+
     shader->bind();
     shader->setUniform("modelview", modelview);
     shader->setUniform("projection", camera.getProjectionMatrix());
     shader->setUniform("normalMatrix", normalMatrix);
+
     shader->setUniform("lightDir", lightDir);
+
+    // these are all given in camera space, since light calculations are easiest there (camera is at 0, 0, 0)
+    shader->setUniform("areaLightPos", glm::vec3(camera.getViewMatrix() * glm::vec4(areaLightPos, 1.0f)));
+    shader->setUniform("areaLightSize", areaLightSize);
+    /*LOG_DEBUG("\n%f, %f, %f\n%f, %f, %f\n%f, %f, %f",
+        areaLightBase[0][0], areaLightBase[0][1], areaLightBase[0][2],
+        areaLightBase[1][0], areaLightBase[1][1], areaLightBase[1][2],
+        areaLightBase[2][0], areaLightBase[2][1], areaLightBase[2][2]);*/
+    shader->setUniform("areaLightBase", glm::mat3(camera.getViewMatrix()) * areaLightBase);
+    shader->setUniform("ambient", 0.1f);
     for(auto mesh : meshes) mesh->draw();
+
+    glm::mat4 lightModel = glm::translate(glm::mat4(), areaLightPos) *
+                           glm::mat4(areaLightBase) *
+                           glm::scale(glm::mat4(), glm::vec3(areaLightSize, 1.0f));
+    shader->setUniform("modelview", camera.getViewMatrix() * lightModel);
+    shader->setUniform("ambient", 1.0f);
+    lightMesh->draw();
+
     shader->unbind();
 }
 
